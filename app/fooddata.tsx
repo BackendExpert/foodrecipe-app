13cb36b3f83ae16a0e09d6e608973a19a81a7f73
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-08 20:53:14 +0530

    Update fooddata.tsx

--- app/fooddata.tsx
+++ app/fooddata.tsx
@@ -1,18 +1,27 @@
 import axios from "axios";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import { Text, View, StyleSheet, ImageBackground, Image, FlatList, TouchableOpacity } from "react-native";
+
+interface Ingredient {
+    name: string;
+    quantity: string;
+}
 
 interface Recipe {
     id: number;
     name: string;
     cuisine: string;
+    instructions: string;
+    ingredients: Ingredient[];
 }
 
 export default function FoodDataScreen() {
     const { id } = useLocalSearchParams();
     const OneFoodBG = require("../assets/images/OneFood.jpg");
     const [oneFoodData, setOneFoodData] = useState<Recipe | null>(null);
+    const pizzaImg = require("../assets/images/Pizza.jpg");
+    const otherfoods = require("../assets/images/OtherFoods.jpg");
 
     useEffect(() => {
         axios.get(`https://jkrecipeapi.vercel.app/api/onerecipe/${id}`)
@@ -30,10 +39,41 @@ export default function FoodDataScreen() {
                 <View style={styles.overlay}>
                     <View style={styles.foodData}>
                         {oneFoodData ? (
-                            <Text style={styles.defaultText}>{oneFoodData.name}</Text>
+                            <View>
+                                <Text style={styles.foodTitle}>{oneFoodData.name}</Text>
+                                <Text style={{ fontSize: 15, color: 'white' }}>{oneFoodData.cuisine}</Text>
+                                <Image
+                                    source={oneFoodData.name.toLowerCase().includes("pizza") ? pizzaImg : otherfoods}
+                                    style={styles.foodImg}
+                                />
+
+                                <View style={styles.foodContent}>
+                                    <View>
+                                        <Text style={styles.foodSubTitle}>Ingredients</Text>
+
+                                        <FlatList 
+                                            data={oneFoodData.ingredients}
+                                            keyExtractor={(item, index) => index.toString()}
+                                            renderItem={({ item }) => (
+                                                <View style={styles.Ingredients}>
+                                                    <Text style={styles.instText}>{item.name}</Text>
+                                                    <Text style={styles.instText}>{item.quantity}</Text>
+                                                </View>
+                                            )}
+                                        />
+                                    </View>
+                                    <View>
+                                        <Text style={styles.foodSubTitle}>Instructions</Text>
+                                        <Text style={styles.instText}>{oneFoodData.instructions}</Text>
+                                    </View>
+
+                                </View>
+                            </View>
                         ) : (
                             <Text style={styles.defaultText}>No data found</Text>
                         )}
+
+
                     </View>
                 </View>
             </ImageBackground>
@@ -64,5 +104,36 @@ const styles = StyleSheet.create({
     foodData: {
         marginTop: 30,
         marginHorizontal: 20,
+    },
+    foodTitle: {
+        color: 'orange',
+        fontSize: 20
+    },
+    foodImg: {
+        width: '100%',
+        height: '30%',
+        marginTop: 20,
+        borderRadius: 20
+    },
+    foodSubTitle: {
+        color: 'orange',
+        fontSize: 20,
+        marginBottom: 10
+    },
+    instText: {
+        color: 'white',
+        fontWeight: 'bold',
+        lineHeight: 24,
+    },
+    foodContent: {
+        marginVertical: 20
+    },
+    Ingredients: {
+        backgroundColor: 'rgba(112,112,112, 0.5)",',
+        padding: 15,
+        borderRadius: 15,
+        marginVertical: 4,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     }
 });
