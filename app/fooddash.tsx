abe3d2a339a49ac81e55c26e18e28cb024fa44a2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-08 10:55:27 +0530

    Update

--- app/fooddash.tsx
+++ app/fooddash.tsx
@@ -1,14 +1,18 @@
 import axios from "axios";
+import { Link } from "expo-router";
 import { useEffect, useState } from "react";
-import { Text, View, StyleSheet, FlatList, ImageBackground } from "react-native";
+import { Text, View, StyleSheet, FlatList, ImageBackground, Image, TouchableOpacity } from "react-native";
 
 interface Recipe {
     id: number;
     name: string;
+    cuisine: string;
 }
 
 export default function FoodDashScreen() {
     const image = require("../assets/images/DashBG.jpg");
+    const pizzaImg = require("../assets/images/Pizza.jpg");
+    const RightArrow = require("../assets/images/RightArrow.png");
     const [apidata, setApiData] = useState<Recipe[]>([]);
 
     useEffect(() => {
@@ -18,29 +22,45 @@ export default function FoodDashScreen() {
             .catch((err) => console.log(err)); 
     }, []);
 
+    const hasPizza = apidata.some(item => item.name.toLowerCase().includes("pizza"));
+
     return (
         <View style={styles.container}>
             <ImageBackground source={image} style={styles.background} resizeMode="cover">
                 <View style={styles.overlay}>
-                    
+                    <View style={styles.dashHeader}>
+                        <Text style={styles.dashHeaderText}>Food Recipes</Text>
+                    </View>
+                    <View style={styles.foodrecipes}>
+                    <FlatList
+                        data={apidata} 
+                        keyExtractor={(item) => item.id.toString()} 
+                        renderItem={({ item }) => {
+                            if (item.name.toLowerCase().includes("pizza")) {
+                                return (
+                                    <Link href={'/fooddash'}>
+
+                                            <TouchableOpacity style={styles.item}>
+                                                <Image source={pizzaImg} style={styles.itemImg}/>
+                                                <View>
+                                                    <Text style={styles.foodRecipeText}>{item.name}</Text> 
+                                                    <View style={styles.recipeData}>
+                                                        <Text style={styles.otherItemText}>{item.cuisine}</Text>
+                                                    </View>
+                                                </View>
+                                            </TouchableOpacity>
+
+                                    </Link>
+                                );
+                            }
+                            return null;
+                        }}
+                    />
+                    </View>
                 </View>
             </ImageBackground>
         </View>
-        // <View style={styles.container}>
-        //     <FlatList
-        //         data={apidata}
-        //         keyExtractor={(item) => item.id.toString()}
-        //         renderItem={({ item }) => (
-        //             <View style={styles.item}>
-        //                 <View>
-        //                     <Text>{item.name}</Text> 
 
-        //                 </View>
-
-        //             </View>
-        //         )}
-        //     />
-        // </View>
     );
 }
 
@@ -48,11 +68,39 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    foodrecipes: {
+        marginTop: 40,
+        marginBottom: 120
+    },
+    otherItemText:{
+        color: 'white'
+    },
     item: {
         padding: 10,
         marginBottom: 10,
-        backgroundColor: "#f8f8f8",
         borderRadius: 5,
+        flexDirection: 'row',
+    },
+    recipeData: {
+        paddingLeft: 20
+    },
+    itemImg:{
+        width: 60,
+        height: 60,
+        borderRadius: 10
+    },
+    itemData: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between'
+    },
+    arrowImg: {
+        width: 20,
+        height: 20,
+    },
+    foodRecipeText: {
+        color: 'orange', 
+        paddingLeft: 20, 
+        fontSize: 20
     },
     background: {
         width: "100%",
@@ -66,4 +114,12 @@ const styles = StyleSheet.create({
         bottom: 0, // Full screen
         backgroundColor: "rgba(0, 0, 0, 0.6)", // Optional translucent overlay
     },
+    dashHeader: {
+        paddingTop: 25
+    },
+    dashHeaderText: {
+        color: 'orange',
+        textAlign: 'center',
+        fontSize: 15
+    }
 });
