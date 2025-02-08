7d53ba7f2939317adee1118d508d8493af4a9261
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-08 21:50:28 +0530

    Update fooddata.tsx

--- app/fooddata.tsx
+++ app/fooddata.tsx
@@ -1,9 +1,25 @@
+import axios from "axios";
 import { Link, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
 import { Text, View, StyleSheet, ImageBackground, Image, TouchableOpacity } from "react-native";
+
+interface Recipe {
+    id: number;
+    name: string;
+    cuisine: string;
+}
 
 export default function FoodDataScreen() {
     const {id} = useLocalSearchParams()
     const oneFoodBG = require("../assets/images/OneFood.jpg");
+    const [oneFood, SetoneFood] = useState<Recipe[]>([]);
+
+    useEffect(() => {
+        axios.get(`https://jkrecipeapi.vercel.app/api/onerecipe/${id}`)
+        .then(res => SetoneFood(res.data.Result))
+        .catch(err => console.log(err)) 
+    }, [])
+    
     return (
         <View style={styles.container}>
             <ImageBackground source={oneFoodBG} style={styles.background} resizeMode="cover">
