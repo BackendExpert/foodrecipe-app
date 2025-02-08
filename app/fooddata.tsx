019fd9434bbe3e47cf844dad626567dfe683f48b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-08 20:23:16 +0530

    Update fooddata.tsx

--- app/fooddata.tsx
+++ app/fooddata.tsx
@@ -1,40 +1,68 @@
+import axios from "axios";
 import { useLocalSearchParams } from "expo-router";
-import { Text, View, StyleSheet, ImageBackground, Image, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet, ImageBackground } from "react-native";
 
-export default function foodDataScreen (){
-    const {id} = useLocalSearchParams()
+interface Recipe {
+    id: number;
+    name: string;
+    cuisine: string;
+}
+
+export default function FoodDataScreen() {
+    const { id } = useLocalSearchParams();
     const OneFoodBG = require("../assets/images/OneFood.jpg");
+    const [oneFoodData, setOneFoodData] = useState<Recipe | null>(null);
+
+    useEffect(() => {
+        axios.get(`https://jkrecipeapi.vercel.app/api/onerecipe/${id}`)
+            .then(res => {
+                setOneFoodData(res.data.Result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [id]);
+
     return (
         <View style={styles.container}>
             <ImageBackground source={OneFoodBG} style={styles.background} resizeMode="cover">
                 <View style={styles.overlay}>
-                    <View>
-                        <Text style={styles.defualtText}>lo</Text>
+                    <View style={styles.foodData}>
+                        {oneFoodData ? (
+                            <Text style={styles.defaultText}>{oneFoodData.name}</Text>
+                        ) : (
+                            <Text style={styles.defaultText}>No data found</Text>
+                        )}
                     </View>
                 </View>
             </ImageBackground>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    defualtText: {
-        color: '#000',
-        fontSize: 20
+    defaultText: {
+        color: '#fff',
+        fontSize: 20,
     },
     overlay: {
-        position: "absolute", // Position the overlay absolutely within the parent
+        position: "absolute",
         top: 0,
         left: 0,
         right: 0,
-        bottom: 0, // Full screen
-        backgroundColor: "rgba(0, 0, 0, 0.8)", // Optional translucent overlay
-    }, 
+        bottom: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.8)",
+    },
     background: {
         width: "100%",
         height: "100%",
+    },
+    foodData: {
+        marginTop: 30,
+        marginHorizontal: 20,
     }
-})
+});
