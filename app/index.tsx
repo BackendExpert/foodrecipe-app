e99b0e72b0482c378fcbfd8c45884b986b85e259
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-08 09:43:42 +0530

    Update

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,53 @@
-import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import { Link } from "expo-router";
+import { Text, View, StyleSheet, ImageBackground, Image, TouchableOpacity } from "react-native";
 
 export default function Index() {
   // Local image reference
   const image = require("../assets/images/WelcomeImg.jpg");
-
+  const logoNew = require("../assets/images/LogoNew.png");
   return (
     <View style={styles.container}>
       <ImageBackground source={image} style={styles.background} resizeMode="cover">
         <View style={styles.overlay}>
-          <Text style={styles.text}>Welcome to TasteBite</Text>
+          <View style={styles.headerpart}>
+            <Text style={styles.headerTest}>Free Food Recipes</Text>
+          </View>
+          <View>
+            
+          </View>
+          <View style={styles.body}>
+            <Text style={styles.bodyTitle}>Lets Cook with</Text>
+            <Image source={logoNew} style={styles.logoStyle}/>
+            <Text style={styles.bodyText}>The Food Recipe App</Text>
+          </View>
+          <View style={styles.footer}>
+            {/* TouchableOpacity for Start Cooking */}
+            <TouchableOpacity>
+              <Link href="/about" style={styles.footerBtn}>
+                <Text style={styles.footbtnText}>Start Cooking</Text>
+              </Link>
+            </TouchableOpacity>
+            <Text style={styles.footercontent}>v1.0.0-10 Feb 2025</Text>            
+            <Text style={styles.footercontent}>
+              Developed using <Link href={'https://github.com/BackendExpert'} style={{ color: "orange"}}>JKRecipeAPI</Link>
+            </Text>
+          </View>
         </View>
+
       </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  logoStyle: {
+    width: '100%',
+    height: '35%'
+  },
   container: {
     flex: 1,
   },
   background: {
-    flex: 1,
     width: "100%",
     height: "100%",
   },
@@ -31,10 +58,60 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0, // Full screen
     backgroundColor: "rgba(0, 0, 0, 0.6)", // Optional translucent overlay
+
+
+  },
+  headerpart: {
+    paddingTop: 40,    
+  },
+  headerTest: {
+    color: "gray",
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  body: {
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
+    marginTop: 400,
     borderRadius: 10,
+  },
+
+  bodyText: {
+    color: "#fff",
+    marginTop: -10,
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  bodyTitle: {
+    color: "#fff",
+    marginTop: -10,
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  footer: {
+    marginHorizontal: 60,
+    marginTop: -20,
+  },
+  footerBtn: {
+    backgroundColor: 'orange',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 12
+  },
+  footbtnText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+
+  footercontent: {
+    color: 'white',
+    fontSize: 10,
+    textAlign: "center"
   },
   text: {
     color: "#fff",
